6d0243af67f4e4ac0a61aab4030e90c96c036ce7
Greice Felipe <greice.n.felipe@gmail.com>
2021-11-29 08:34:13 -0300

    Primeiro commit - Tabela dinamica.

--- tabela-dinamica-refeicoes/ViewController.swift
+++ tabela-dinamica-refeicoes/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let refeicoes = ["Churros", "Macarrão", "Pizza"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("TableViewController foi carregado")
     }
-
+    
+    // A função diz ao Table View quantas linhas nossa tabela deve ter.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    // A função diz ao Table View o conteudo da linha da nossa tabela
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        celula.textLabel?.text = refeicao
+        
+        return celula
+    }
 
 }
 
